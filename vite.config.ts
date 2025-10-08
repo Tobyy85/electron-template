0e6b2e0ba92580f72c34475b206a12d83ff8c5fd
Tobias <154422539+Tobyy85@users.noreply.github.com>
2025-10-08 19:31:26 +0200

    fix: update base URL and server settings in vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: '/VAR_BASE_URL',
+    base: './',
+    server: {
+        port: 3000,
+        strictPort: true,
+    },
     resolve: {
         alias: {
             '@': '/src',
