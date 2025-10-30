a095ae4d3c1ec3d52ecf48febee3df6389344469
Tobias <154422539+Tobyy85@users.noreply.github.com>
2025-10-30 18:47:52 +0100

    feat(docker): add Dockerfile and docker-compose

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:22
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+ENV PORT=5173
+EXPOSE ${PORT}
+
+CMD ["npm", "run", "dev"]
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     base: '/VAR_BASE_URL',
+    server: {
+        host: '0.0.0.0',
+        port: parseInt(process.env.PORT || '5173'),
+        strictPort: true,
+        watch: {
+            usePolling: process.env.CHOKIDAR_USEPOLLING === 'true',
+            interval: 200,
+        },
+    },
     resolve: {
         alias: {
             '@': '/src',
