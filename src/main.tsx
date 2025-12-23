10b2a4cda6735b33d5ef1f2c8d931da24c564d08
Tobias <154422539+Tobyy85@users.noreply.github.com>
2025-12-23 23:07:42 +0100

    feat(routes): move route definitions to separate file

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import { routes } from '@/routes'
+
 import './styles/tailwind.css'
 
-import HomePage from '@/pages/HomePage'
-import NotFoundPage from '@/pages/NotFoundPage'
-
-const router = createBrowserRouter(
-    [
-        {
-            path: '/',
-            element: <HomePage />,
-            errorElement: <NotFoundPage />,
-        },
-    ],
-    { basename: import.meta.env.BASE_URL || '/' }
-)
+const router = createBrowserRouter(routes, { basename: import.meta.env.BASE_URL || '/' })
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
